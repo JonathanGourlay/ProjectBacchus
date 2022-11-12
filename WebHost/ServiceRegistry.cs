e28a7cf6105b7aef81847eda8168e9ec964b4ba7
JonathanGourlay <48126931+JonathanGourlay@users.noreply.github.com>
2022-11-12 23:45:57 +0000

    Initial commit

---
+++ WebHost/ServiceRegistry.cs
@@ -0,0 +1,37 @@
+﻿using Net6SpaTemplate.BLL;
+using Net6SpaTemplate.DAL;
+using Scrutor;
+using System.CodeDom.Compiler;
+
+namespace Net6SpaTemplate
+{
+    public static class ServiceRegistry
+    {
+        public static void ScanForAllRemainingRegistrations(IServiceCollection services)
+        {
+            services.Scan(scan => scan
+                .FromAssembliesOf(typeof(Program), typeof(TestFacade), typeof(TestRepository))
+                .AddClasses(x => x.WithoutAttribute<GeneratedCodeAttribute>())
+                .UsingRegistrationStrategy(RegistrationStrategy.Skip)
+                .AsImplementedInterfaces()
+                .WithScopedLifetime());
+        }
+
+        public static void AddConfig(IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<AppSettings>(provider => configuration.GetSection("AppConfig").Bind(provider));
+        }
+
+        public static void AddForwardedHeaderOptions(IServiceCollection services)
+        {
+         
+        }
+
+        public static void AddRestClients(IServiceCollection services, IConfiguration configuration)
+        {
+            
+        }
+
+    }
+}
+
